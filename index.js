bbd01182d10ebe85d9a5a2ac4f1b17b2fb4b649b
NadyaYak <122393665+NadyaYak@users.noreply.github.com>
2023-02-26 20:22:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let num = 80
+
+ if(num >=90){
+  console.log ("A")
+}
+
+ else if(num >=80 && num < 90){
+  
+  console.log("B")
+}
+else if(num >= 70 && num < 80){
+  
+  console.log("C")
+}
+else if(num > 55 && num < 70){
+  
+  console.log("D")
+}
+else{
+  console.log("F")
+}
+
